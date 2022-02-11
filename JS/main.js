d62de675370b0391c84c2dc7a66aa5f9c1e49fa4
gauravkumar5089 <72368836+gauravkumar5089@users.noreply.github.com>
2022-02-11 23:55:39 +0530

    Added JS for sidebar

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,10 @@
+// SideBar Toggle
+const toggleBtn = document.getElementById("bars").onclick = function(){
+  let subHeader = document.getElementById("sub-header");
+  subHeader.classList.toggle("hide");
+  console.log(subHeader);
+}
+
 const API_key = "api_key=5d04012bc4d5ad4443c7e8a9c0b416ac";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
@@ -6,70 +13,70 @@ const searchURL = BASE_URL + '/search/movie?' + API_key;
 
 const popular1 = BASE_URL + "/discover/movie?sort_by=popularity.des&page=1&" + API_key;
 const popular2 = BASE_URL + "/discover/movie?sort_by=popularity.des&page=2&" + API_key;
-const topRated =BASE_URL + "/discover/movie/?sort_by=vote_average.desc&" +API_key;
+const topRated = BASE_URL + "/discover/movie/?sort_by=vote_average.desc&" + API_key;
 const upcoming = BASE_URL + "/discover/movie?primary_release_date.gte=2022-02-10&primary_release_date.lte=2022-03-31&" + API_key;
 // const anime = "https://api.jikan.moe/v4/anime?q=&sfw";
 // const tv_shows = BASE_URL + "/discover/movie?with_genres=10770&" + API_key;
 // const webseries =BASE_URL + "/discover/movie?sort_by=popularity.des&" + API_key;
 
 
-const Action1 = BASE_URL + "/discover/movie?with_genres=28&page=1&" +API_key;
-const Action2 = BASE_URL + "/discover/movie?with_genres=28&page=2&" +API_key;
-const Action3 = BASE_URL + "/discover/movie?with_genres=28&page=3&" +API_key;
-const Adventure1 = BASE_URL + "/discover/movie?with_genres=12&page=1&" +API_key;
-const Adventure2 = BASE_URL + "/discover/movie?with_genres=12&page=2&" +API_key;
-const Adventure3 = BASE_URL + "/discover/movie?with_genres=12&page=3&" +API_key;
-const Comedy1 = BASE_URL + "/discover/movie?with_genres=35&page=1&" +API_key;
-const Comedy2 = BASE_URL + "/discover/movie?with_genres=35&page=2&" +API_key;
-const Comedy3 = BASE_URL + "/discover/movie?with_genres=35&page=3&" +API_key;
-const Crime1 = BASE_URL + "/discover/movie?with_genres=80&page=1&" +API_key;
-const Crime2 = BASE_URL + "/discover/movie?with_genres=80&page=2&" +API_key;
-const Crime3 = BASE_URL + "/discover/movie?with_genres=80&page=3&" +API_key;
-const Drama1 = BASE_URL + "/discover/movie?with_genres=18&page=1&" +API_key;
-const Drama2 = BASE_URL + "/discover/movie?with_genres=18&page=2&" +API_key;
-const Drama3 = BASE_URL + "/discover/movie?with_genres=18&page=3&" +API_key;
-const Family1 = BASE_URL + "/discover/movie?with_genres=14&page=1&" +API_key;
-const Family2 = BASE_URL + "/discover/movie?with_genres=14&page=2&" +API_key;
-const Family3 = BASE_URL + "/discover/movie?with_genres=14&page=3&" +API_key;
-const Documentary1 = BASE_URL + "/discover/movie?with_genres=99&page=1&" +API_key;
-const Documentary2 = BASE_URL + "/discover/movie?with_genres=99&page=2&" +API_key;
-const Documentary3 = BASE_URL + "/discover/movie?with_genres=99&page=3&" +API_key;
-const Horror1 = BASE_URL + "/discover/movie?with_genres=27&page=1&" +API_key;
-const Horror2 = BASE_URL + "/discover/movie?with_genres=27&page=2&" +API_key;
-const Horror3 = BASE_URL + "/discover/movie?with_genres=27&page=3&" +API_key;
-const Sci1 = BASE_URL + "/discover/movie?with_genres=878&page=1&" +API_key;
-const Sci2 = BASE_URL + "/discover/movie?with_genres=878&page=2&" +API_key;
-const Sci3 = BASE_URL + "/discover/movie?with_genres=878&page=3&" +API_key;
-const Romance1 = BASE_URL + "/discover/movie?with_genres=10749&page=1&" +API_key;
-const Romance2 = BASE_URL + "/discover/movie?with_genres=10749&page=2&" +API_key;
-const Romance3 = BASE_URL + "/discover/movie?with_genres=10749&page=3&" +API_key;
-const Thriller1 = BASE_URL + "/discover/movie?with_genres=53&page=1&" +API_key;
-const Thriller2 = BASE_URL + "/discover/movie?with_genres=53&page=2&" +API_key;
-const Thriller3 = BASE_URL + "/discover/movie?with_genres=53&page=3&" +API_key;
+const Action1 = BASE_URL + "/discover/movie?with_genres=28&page=1&" + API_key;
+const Action2 = BASE_URL + "/discover/movie?with_genres=28&page=2&" + API_key;
+const Action3 = BASE_URL + "/discover/movie?with_genres=28&page=3&" + API_key;
+const Adventure1 = BASE_URL + "/discover/movie?with_genres=12&page=1&" + API_key;
+const Adventure2 = BASE_URL + "/discover/movie?with_genres=12&page=2&" + API_key;
+const Adventure3 = BASE_URL + "/discover/movie?with_genres=12&page=3&" + API_key;
+const Comedy1 = BASE_URL + "/discover/movie?with_genres=35&page=1&" + API_key;
+const Comedy2 = BASE_URL + "/discover/movie?with_genres=35&page=2&" + API_key;
+const Comedy3 = BASE_URL + "/discover/movie?with_genres=35&page=3&" + API_key;
+const Crime1 = BASE_URL + "/discover/movie?with_genres=80&page=1&" + API_key;
+const Crime2 = BASE_URL + "/discover/movie?with_genres=80&page=2&" + API_key;
+const Crime3 = BASE_URL + "/discover/movie?with_genres=80&page=3&" + API_key;
+const Drama1 = BASE_URL + "/discover/movie?with_genres=18&page=1&" + API_key;
+const Drama2 = BASE_URL + "/discover/movie?with_genres=18&page=2&" + API_key;
+const Drama3 = BASE_URL + "/discover/movie?with_genres=18&page=3&" + API_key;
+const Family1 = BASE_URL + "/discover/movie?with_genres=14&page=1&" + API_key;
+const Family2 = BASE_URL + "/discover/movie?with_genres=14&page=2&" + API_key;
+const Family3 = BASE_URL + "/discover/movie?with_genres=14&page=3&" + API_key;
+const Documentary1 = BASE_URL + "/discover/movie?with_genres=99&page=1&" + API_key;
+const Documentary2 = BASE_URL + "/discover/movie?with_genres=99&page=2&" + API_key;
+const Documentary3 = BASE_URL + "/discover/movie?with_genres=99&page=3&" + API_key;
+const Horror1 = BASE_URL + "/discover/movie?with_genres=27&page=1&" + API_key;
+const Horror2 = BASE_URL + "/discover/movie?with_genres=27&page=2&" + API_key;
+const Horror3 = BASE_URL + "/discover/movie?with_genres=27&page=3&" + API_key;
+const Sci1 = BASE_URL + "/discover/movie?with_genres=878&page=1&" + API_key;
+const Sci2 = BASE_URL + "/discover/movie?with_genres=878&page=2&" + API_key;
+const Sci3 = BASE_URL + "/discover/movie?with_genres=878&page=3&" + API_key;
+const Romance1 = BASE_URL + "/discover/movie?with_genres=10749&page=1&" + API_key;
+const Romance2 = BASE_URL + "/discover/movie?with_genres=10749&page=2&" + API_key;
+const Romance3 = BASE_URL + "/discover/movie?with_genres=10749&page=3&" + API_key;
+const Thriller1 = BASE_URL + "/discover/movie?with_genres=53&page=1&" + API_key;
+const Thriller2 = BASE_URL + "/discover/movie?with_genres=53&page=2&" + API_key;
+const Thriller3 = BASE_URL + "/discover/movie?with_genres=53&page=3&" + API_key;
 
 const trendingSwiper = document.getElementById("trending-movies");
-trendingSwiper.innerHTML="";
+trendingSwiper.innerHTML = "";
 const topRatedSection = document.getElementById("top-rated");
-topRatedSection.innerHTML="";
+topRatedSection.innerHTML = "";
 const animeSection = document.getElementById("anime");
-animeSection.innerHTML="";
+animeSection.innerHTML = "";
 const upcomingSection = document.getElementById("upcoming");
-upcomingSection.innerHTML="";
+upcomingSection.innerHTML = "";
 const webseriesSection = document.getElementById("webseries");
-webseriesSection.innerHTML="";
+webseriesSection.innerHTML = "";
 const tv_showsSection = document.getElementById("tv-shows");
-tv_showsSection.innerHTML="";
+tv_showsSection.innerHTML = "";
 
 const section = document.getElementById("Other");
-section.innerHTML="";
+section.innerHTML = "";
 
 const main = document.getElementById("main");
 
 const form = document.getElementById("form");
 const search = document.getElementById("search-box");
 
-getMoviesInSlider(popular1,trendingSwiper);
-getMoviesInSlider(popular2,trendingSwiper);
+getMoviesInSlider(popular1, trendingSwiper);
+getMoviesInSlider(popular2, trendingSwiper);
 getmovies(topRated, topRatedSection);
 getmovies(upcoming, upcomingSection);
 //getmovies(tv_shows, tv_showsSection);
@@ -78,7 +85,7 @@ getmovies(upcoming, upcomingSection);
 
 document.getElementById("action").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Action</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Action1, section);
   getmovies(Action2, section);
   getmovies(Action3, section);
@@ -86,7 +93,7 @@ document.getElementById("action").onmousedown = function () {
 
 document.getElementById("adventure").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Adventure</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Adventure1, section);
   getmovies(Adventure2, section);
   getmovies(Adventure3, section);
@@ -94,7 +101,7 @@ document.getElementById("adventure").onmousedown = function () {
 
 document.getElementById("comedy").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Comedy</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Comedy1, section);
   getmovies(Comedy2, section);
   getmovies(Comedy3, section);
@@ -102,7 +109,7 @@ document.getElementById("comedy").onmousedown = function () {
 
 document.getElementById("crime").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Crime</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Crime1, section);
   getmovies(Crime2, section);
   getmovies(Crime3, section);
@@ -110,7 +117,7 @@ document.getElementById("crime").onmousedown = function () {
 
 document.getElementById("drama").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Drama</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Drama1, section);
   getmovies(Drama2, section);
   getmovies(Drama3, section);
@@ -118,7 +125,7 @@ document.getElementById("drama").onmousedown = function () {
 
 document.getElementById("family").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Family</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Family1, section);
   getmovies(Family2, section);
   getmovies(Family3, section);
@@ -126,7 +133,7 @@ document.getElementById("family").onmousedown = function () {
 
 document.getElementById("documentary").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Documentry</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Documentary1, section);
   getmovies(Documentary2, section);
   getmovies(Documentary3, section);
@@ -134,7 +141,7 @@ document.getElementById("documentary").onmousedown = function () {
 
 document.getElementById("horror").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Horror</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Horror1, section);
   getmovies(Horror2, section);
   getmovies(Horror3, section);
@@ -142,7 +149,7 @@ document.getElementById("horror").onmousedown = function () {
 
 document.getElementById("sci-fi").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Sci-fi</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Sci1, section);
   getmovies(Sci2, section);
   getmovies(Sci3, section);
@@ -150,7 +157,7 @@ document.getElementById("sci-fi").onmousedown = function () {
 
 document.getElementById("romance").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Romance</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Romance1, section);
   getmovies(Romance2, section);
   getmovies(Romance3, section);
@@ -158,37 +165,37 @@ document.getElementById("romance").onmousedown = function () {
 
 document.getElementById("thriller").onmousedown = function () {
   main.innerHTML = '<h1 class = "heading" >Thriller</h1>';
-  section.innerHTML="";
+  section.innerHTML = "";
   getmovies(Thriller1, section);
   getmovies(Thriller2, section);
   getmovies(Thriller3, section);
 };
 
-document.getElementById("search-button").onclick = function() {
+document.getElementById("search-button").onclick = function () {
   //e.preventDefault;
   const searchTerm = search.value;
-  if(searchTerm){
-    section.innerHTML="";
-    getmovies(searchURL+'&query='+searchTerm+'&page=1',section);
-    getmovies(searchURL+'&query='+searchTerm+'&page=2',section);
-    getmovies(searchURL+'&query='+searchTerm+'&page=3',section);
-    getmovies(searchURL+'&query='+searchTerm+'&page=4',section);
-    getmovies(searchURL+'&query='+searchTerm+'&page=5',section);
-    main.innerHTML = `<h1 class = "heading" >${searchTerm}</h1>`;
-  }else{
+  if (searchTerm) {
+    section.innerHTML = "";
+    getmovies(searchURL + '&query=' + searchTerm + '&page=1', section);
+    getmovies(searchURL + '&query=' + searchTerm + '&page=2', section);
+    getmovies(searchURL + '&query=' + searchTerm + '&page=3', section);
+    getmovies(searchURL + '&query=' + searchTerm + '&page=4', section);
+    getmovies(searchURL + '&query=' + searchTerm + '&page=5', section);
+    main.innerHTML = `<h1 class = "heading" >Search results for "${searchTerm}"</h1>`;
+  } else {
     window.location.reload();
   }
 };
 
-function getMoviesInSlider(url,show_type) {
+function getMoviesInSlider(url, show_type) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      showMoviesInSlider(data.results,show_type);
+      showMoviesInSlider(data.results, show_type);
     });
 }
 
-function showMoviesInSlider(data,show_type) {
+function showMoviesInSlider(data, show_type) {
   data.forEach((movie) => {
     const { title, poster_path, vote_average, release_date, genre_ids } = movie;
     const swiperSlide = document.createElement("div");
@@ -222,14 +229,14 @@ function getmovies(url, movie_type) {
 }
 
 function showmovies(data, movie_type) {
-  data.forEach((movie)=> {
+  data.forEach((movie) => {
     const { title, poster_path, vote_average, release_date, genre_ids } = movie;
     const movieCard = document.createElement("div");
     const release_year = release_date.substr(0, 4);
     const g_id = genre_ids[0];
     movieCard.classList.add("movie-card");
     movieCard.classList.add("scale");
-    if(movie_type == section){
+    if (movie_type == section) {
       movieCard.innerHTML = `
       <div class="movie-poster">      
           <img src="${IMG_URL + poster_path}" alt="${title}">
@@ -238,11 +245,11 @@ function showmovies(data, movie_type) {
               <div class="movie-year">${release_year}</div>
               <div class="movie-name">${title}</div>
               <div class="movie-rating"><span class="${getcolor(
-                vote_average
-              )}">${vote_average}</span><i class="fas fa-star star"></i></div>
-      </div>`;  
-    }else{
-    movieCard.innerHTML = `
+        vote_average
+      )}">${vote_average}</span><i class="fas fa-star star"></i></div>
+      </div>`;
+    } else {
+      movieCard.innerHTML = `
     <div class="movie-poster">      
         <img src="${IMG_URL + poster_path}" alt="${title}">
     </div>
@@ -251,8 +258,8 @@ function showmovies(data, movie_type) {
             <div class="movie-name">${title}</div>
             <div class="movie-genre">${getgenre(g_id)}</div>
             <div class="movie-rating"><span class="${getcolor(
-              vote_average
-            )}">${vote_average}</span><i class="fas fa-star star"></i></div>
+        vote_average
+      )}">${vote_average}</span><i class="fas fa-star star"></i></div>
     </div>`;
     }
     movie_type.appendChild(movieCard);
@@ -262,9 +269,9 @@ function showmovies(data, movie_type) {
 function getcolor(vote) {
   if (vote >= 7.5) {
     return "green";
-  }else if (vote >= 5) {
+  } else if (vote >= 5) {
     return "orange";
-  }else{
+  } else {
     return "red";
   }
 }
